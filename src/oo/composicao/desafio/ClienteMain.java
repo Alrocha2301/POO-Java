b16d573f747e1e7c6aa7004e166d57be21bc2c33
aepc <alexandre-apr@hotmail.com>
2024-03-22 17:29:46 -0300

    second commit

--- src/oo/composicao/desafio/ClienteMain.java
+++ src/oo/composicao/desafio/ClienteMain.java
@@ -5,6 +5,7 @@ public class ClienteMain {
 
         Cliente cliente1 = new Cliente("Alexandre");
         Compra compra1 = new Compra(cliente1);
+        var compra2 = new Compra(cliente1);
 
         Produto p1 = new Produto("Lápis", 1.3);
         var p2 = new Produto("Caneta", 2.5);
@@ -16,9 +17,18 @@ public class ClienteMain {
         compra1.adicionarItem(p2, 3);
         compra1.adicionarItem(p3, 1);
 
-        System.out.printf("Valor total gasto por %s: R$ %.2f\n", cliente1.nome, cliente1.totalGasto());
+        compra2.adicionarItem(p1, 9);
+        compra2.adicionarItem(p2, 4);
+        compra2.adicionarItem(p3, 2);
 
-        System.out.println(cliente1.mostrarProdutos(compra1));
+
+        System.out.print(cliente1.mostrarProdutos(compra1));
+        System.out.printf("Valor compra 1: R$ %.2f\n", compra1.valorCompra());
+
+        System.out.print(cliente1.mostrarProdutos(compra2));
+        System.out.printf("Valor compra 2: R$ %.2f\n", compra2.valorCompra());
+
+        System.out.printf("Valor total gasto por %s: R$ %.2f\n", cliente1.nome, cliente1.totalGasto());
 
 
 
